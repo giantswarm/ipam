7574bc854d39dae90b528f256a31d44d4aeb03b0
Joseph Salisbury <salisbury.joseph@gmail.com>
2017-08-07 14:54:39 +0100

    Adds NewSubnet method

--- service.go
+++ service.go
@@ -1,10 +1,19 @@
 package ipam
 
 import (
+	"context"
+	"fmt"
+	"net"
+
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	"github.com/giantswarm/microstorage"
 	"github.com/giantswarm/microstorage/memory"
+)
+
+const (
+	IPAMSubnetStorageKey       = "/ipam/subnet"
+	IPAMSubnetStorageKeyFormat = "/ipam/subnet/%s"
 )
 
 // Config represents the configuration used to create a new ipam service.
@@ -12,6 +21,12 @@ type Config struct {
 	// Dependencies.
 	Logger  micrologger.Logger
 	Storage microstorage.Storage
+
+	// Settings.
+	// Network is the network in which all returned subnets should exist.
+	Network *net.IPNet
+	// Mask is the mask for all returned subnets.
+	Mask *net.IPMask
 }
 
 // DefaultConfig provides a default configuration to create a new ipam service
@@ -54,10 +69,22 @@ func New(config Config) (*Service, error) {
 		return nil, microerror.Maskf(invalidConfigError, "storage must not be empty")
 	}
 
+	// Settings.
+	if config.Network == nil {
+		return nil, microerror.Maskf(invalidConfigError, "network must not be empty")
+	}
+	if config.Mask == nil {
+		return nil, microerror.Maskf(invalidConfigError, "mask must not be empty")
+	}
+
 	newService := &Service{
 		// Dependencies.
 		logger:  config.Logger,
 		storage: config.Storage,
+
+		// Settings.
+		network: *config.Network,
+		mask:    *config.Mask,
 	}
 
 	return newService, nil
@@ -67,4 +94,54 @@ type Service struct {
 	// Dependencies.
 	logger  micrologger.Logger
 	storage microstorage.Storage
+
+	// Settings.
+	network net.IPNet
+	mask    net.IPMask
+}
+
+// NewSubnet returns the next available subnet, of the configured size,
+// from the configured network.
+func (s *Service) NewSubnet() (net.IPNet, error) {
+	s.logger.Log("info", "creating new subnet")
+
+	ctx := context.Background()
+
+	s.logger.Log("info", "fetching existing subnets")
+
+	// Fetch existing subnets from storage.
+	existingSubnetStrings, err := s.storage.List(ctx, IPAMSubnetStorageKey)
+	if err != nil && !microstorage.IsNotFound(err) {
+		return net.IPNet{}, microerror.Mask(err)
+	}
+
+	existingSubnets := []net.IPNet{}
+	for _, existingSubnetString := range existingSubnetStrings {
+		_, existingSubnet, err := net.ParseCIDR(existingSubnetString)
+		if err != nil {
+			return net.IPNet{}, microerror.Mask(err)
+		}
+		existingSubnets = append(existingSubnets, *existingSubnet)
+	}
+
+	s.logger.Log("info", "computing next subnet")
+
+	// Compute the next subnet.
+	subnet, err := Free(s.network, s.mask, existingSubnets)
+	if err != nil {
+		return net.IPNet{}, microerror.Mask(err)
+	}
+
+	s.logger.Log("info", "storing computed subnet")
+
+	// Store the next subnet.
+	if err := s.storage.Put(
+		ctx,
+		fmt.Sprintf(IPAMSubnetStorageKeyFormat, subnet.String()),
+		subnet.String(),
+	); err != nil {
+		return net.IPNet{}, microerror.Mask(err)
+	}
+
+	return subnet, nil
 }
--- service_test.go
+++ service_test.go
@@ -1,16 +1,31 @@
 package ipam
 
-import "testing"
+import (
+	"bytes"
+	"net"
+	"testing"
+)
 
-// TestNew tests the New method.
+// TestNew tests the New function.
 func TestNew(t *testing.T) {
+	// testNetwork is a network to set for testing.
+	_, testNetwork, _ := net.ParseCIDR("10.4.0.0/16")
+	// testMask is a mask to set for testing.
+	testMask := net.CIDRMask(16, 32)
+
 	tests := []struct {
 		config               func() Config
 		expectedErrorHandler func(error) bool
 	}{
-		// Test that the default config returns a new IPAM service.
+		// Test that the default config, with a network and mask set,
+		// returns a new IPAM service.
 		{
-			config: DefaultConfig,
+			config: func() Config {
+				c := DefaultConfig()
+				c.Network = testNetwork
+				c.Mask = &testMask
+				return c
+			},
 		},
 
 		// Test that a config with a nil logger returns an invalid config error.
@@ -18,6 +33,8 @@ func TestNew(t *testing.T) {
 			config: func() Config {
 				c := DefaultConfig()
 				c.Logger = nil
+				c.Network = testNetwork
+				c.Mask = &testMask
 				return c
 			},
 			expectedErrorHandler: IsInvalidConfig,
@@ -28,6 +45,30 @@ func TestNew(t *testing.T) {
 			config: func() Config {
 				c := DefaultConfig()
 				c.Storage = nil
+				c.Network = testNetwork
+				c.Mask = &testMask
+				return c
+			},
+			expectedErrorHandler: IsInvalidConfig,
+		},
+
+		// Test that a config with an empty network returns an invalid config error.
+		{
+			config: func() Config {
+				c := DefaultConfig()
+				c.Network = nil
+				c.Mask = &testMask
+				return c
+			},
+			expectedErrorHandler: IsInvalidConfig,
+		},
+
+		// Test that a config with an empty mask returns an invalid config error.
+		{
+			config: func() Config {
+				c := DefaultConfig()
+				c.Network = testNetwork
+				c.Mask = nil
 				return c
 			},
 			expectedErrorHandler: IsInvalidConfig,
@@ -57,6 +98,110 @@ func TestNew(t *testing.T) {
 		} else {
 			if service == nil {
 				t.Fatalf("%v: service is nil", index)
+			}
+		}
+	}
+}
+
+// TestNewSubnet tests the NewSubnet method.
+func TestNewSubnet(t *testing.T) {
+	tests := []struct {
+		network              string
+		mask                 int
+		expectedSubnets      []string
+		expectedErrorHandler func(error) bool
+	}{
+		// Test that the first subnet is returned correctly.
+		{
+			network:         "10.4.0.0/16",
+			mask:            24,
+			expectedSubnets: []string{"10.4.0.0/24"},
+		},
+
+		// Test that two subnets are returned correctly.
+		{
+			network: "10.4.0.0/16",
+			mask:    24,
+			expectedSubnets: []string{
+				"10.4.0.0/24",
+				"10.4.1.0/24",
+			},
+		},
+
+		// Test that three subnets are returned correctly, with a different mask.
+		{
+			network: "10.4.0.0/18",
+			mask:    25,
+			expectedSubnets: []string{
+				"10.4.0.0/25",
+				"10.4.0.128/25",
+				"10.4.1.0/25",
+			},
+		},
+
+		// Test that errors are returned correctly.
+		{
+			network: "10.4.0.0/16",
+			mask:    15,
+			// expect one subnet, even though we're expecting an error, so we actually call `NewSubnet`,
+			expectedSubnets:      []string{"10.4.0.0/16"},
+			expectedErrorHandler: IsMaskTooBig,
+		},
+	}
+
+	for index, test := range tests {
+		// Parse network and mask.
+		_, network, err := net.ParseCIDR(test.network)
+		if err != nil {
+			t.Fatalf("%v: error returned parsing network cidr: %v", index, err)
+		}
+
+		mask := net.CIDRMask(test.mask, 32)
+
+		// Create a new IPAM service.
+		config := DefaultConfig()
+		config.Network = network
+		config.Mask = &mask
+
+		service, err := New(config)
+		if err != nil {
+			t.Fatalf("%v: error returned creating ipam service: %v", index, err)
+		}
+
+		// Parse expected subnets.
+		expectedSubnets := []net.IPNet{}
+		for _, expectedSubnet := range test.expectedSubnets {
+			_, subnet, err := net.ParseCIDR(expectedSubnet)
+			if err != nil {
+				t.Fatalf("%v: error returned parsing expected subnet: %v", index, err)
+			}
+			expectedSubnets = append(expectedSubnets, *subnet)
+		}
+
+		// For each expected subnet, test that it is what the IPAM service returns.
+		for _, expectedSubnet := range expectedSubnets {
+			returnedSubnet, err := service.NewSubnet()
+
+			if err == nil && test.expectedErrorHandler != nil {
+				t.Fatalf("%v: expected error not returned", index)
+			}
+			if err != nil {
+				if test.expectedErrorHandler == nil {
+					t.Fatalf("%v: unexpected error returned: %v", index, err)
+				} else {
+					if !test.expectedErrorHandler(err) {
+						t.Fatalf("%v: incorrect error returned: %v", index, err)
+					}
+				}
+			} else {
+				if !returnedSubnet.IP.Equal(expectedSubnet.IP) || !bytes.Equal(returnedSubnet.Mask, expectedSubnet.Mask) {
+					t.Fatalf(
+						"%v: returned subnet did not match expected.\nexpected: %v\nreturned: %v\n",
+						index,
+						expectedSubnet,
+						returnedSubnet,
+					)
+				}
 			}
 		}
 	}
