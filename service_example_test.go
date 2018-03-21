442e28b638045e8dcbe75dd596241590ded3b0f7
Paweł Kopiczko <kopiczko@gmail.com>
2018-03-21 14:29:57 +0100

    ipam: NewSubnet -> CreateSubnet taking ctx & annotation (#12)

--- service_example_test.go
+++ service_example_test.go
@@ -1,6 +1,7 @@
 package ipam
 
 import (
+	"context"
 	"fmt"
 	"net"
 
@@ -11,6 +12,8 @@ import (
 // This example demonstrates how the IPAM service can be used to dynamically
 // manage subnets within a larger network.
 func Example() {
+	ctx := context.Background()
+
 	// Construct a new IPAM service.
 	logger := microloggertest.New()
 	storage, _ := memory.New(memory.Config{})
@@ -26,30 +29,30 @@ func Example() {
 
 	// Request a subnet. There are no subnets in the network currently,
 	// so this subnet will be at the start of the IP range.
-	firstNetwork, _ := service.NewSubnet(net.CIDRMask(24, 32))
+	firstNetwork, _ := service.CreateSubnet(ctx, net.CIDRMask(24, 32), "arbitrary value")
 	fmt.Println(firstNetwork.String())
 
 	// Request a second, smaller subnet.
 	// There is one subnet currently, so this subnet will begin after
 	// the previous subnet.
-	secondNetwork, _ := service.NewSubnet(net.CIDRMask(32, 32))
+	secondNetwork, _ := service.CreateSubnet(ctx, net.CIDRMask(32, 32), "")
 	fmt.Println(secondNetwork.String())
 
 	// Release the first subnet from the service.
 	// This makes the IP range available for future operations.
-	service.DeleteSubnet(firstNetwork)
+	service.DeleteSubnet(ctx, firstNetwork)
 
 	// Request a third subnet.
 	// As the range at the start of the network is free,
 	// and this subnet fits in the space,
 	// it will be placed there.
-	thirdNetwork, _ := service.NewSubnet(net.CIDRMask(25, 32))
+	thirdNetwork, _ := service.CreateSubnet(ctx, net.CIDRMask(25, 32), "")
 	fmt.Println(thirdNetwork.String())
 
 	// Request a fourth subnet.
 	// As 2 /25s fit within a /24, both this subnet and the previous
 	// subnet fit in the space of the older /24.
-	fourthNetwork, _ := service.NewSubnet(net.CIDRMask(25, 32))
+	fourthNetwork, _ := service.CreateSubnet(ctx, net.CIDRMask(25, 32), "")
 	fmt.Println(fourthNetwork.String())
 
 	// Output:
