79a61ffad296f3f418d8f381e4f5f702d5245b50
Tuomas Mäkinen <1947505+tuommaki@users.noreply.github.com>
2018-11-06 08:51:12 +0200

    Allow submission of reserved networks outside of internal storage (#19)
    IPAM Service maintains its allocations within its own storage, but there
    are use cases when it's needed to be able to submit additional network
    reservations from outside. One example is when migrating IPAM from one
    component to another and changing storage while doing this.
    
    In order to properly merge list of networks from different sources,
    a function to canonicalize list of networks was added here.

--- ipam_test.go
+++ ipam_test.go
@@ -897,3 +897,204 @@ func ExampleHalf() {
 	// 10.4.128.0/17
 
 }
+
+func Test_CanonicalizeSubnets(t *testing.T) {
+	testCases := []struct {
+		name            string
+		network         net.IPNet
+		subnets         []net.IPNet
+		expectedSubnets []net.IPNet
+	}{
+		{
+			name:            "case 0: deduplicate empty list of subnets",
+			network:         mustParseCIDR("192.168.0.0/16"),
+			subnets:         []net.IPNet{},
+			expectedSubnets: []net.IPNet{},
+		},
+		{
+			name:    "case 1: deduplicate list of subnets with one element",
+			network: mustParseCIDR("192.168.0.0/16"),
+			subnets: []net.IPNet{
+				mustParseCIDR("192.168.2.0/24"),
+			},
+			expectedSubnets: []net.IPNet{
+				mustParseCIDR("192.168.2.0/24"),
+			},
+		},
+		{
+			name:    "case 2: deduplicate list of subnets with two non-overlapping elements",
+			network: mustParseCIDR("192.168.0.0/16"),
+			subnets: []net.IPNet{
+				mustParseCIDR("192.168.1.0/24"),
+				mustParseCIDR("192.168.2.0/24"),
+			},
+			expectedSubnets: []net.IPNet{
+				mustParseCIDR("192.168.1.0/24"),
+				mustParseCIDR("192.168.2.0/24"),
+			},
+		},
+		{
+			name:    "case 3: deduplicate list of subnets with two overlapping elements",
+			network: mustParseCIDR("192.168.0.0/16"),
+			subnets: []net.IPNet{
+				mustParseCIDR("192.168.1.0/24"),
+				mustParseCIDR("192.168.1.0/24"),
+			},
+			expectedSubnets: []net.IPNet{
+				mustParseCIDR("192.168.1.0/24"),
+			},
+		},
+		{
+			name:    "case 4: deduplicate list of subnets with four elements where two overlap",
+			network: mustParseCIDR("192.168.0.0/16"),
+			subnets: []net.IPNet{
+				mustParseCIDR("192.168.1.0/24"),
+				mustParseCIDR("192.168.2.0/24"),
+				mustParseCIDR("192.168.2.0/24"),
+				mustParseCIDR("192.168.3.0/24"),
+			},
+			expectedSubnets: []net.IPNet{
+				mustParseCIDR("192.168.1.0/24"),
+				mustParseCIDR("192.168.2.0/24"),
+				mustParseCIDR("192.168.3.0/24"),
+			},
+		},
+		{
+			name:    "case 5: same as case 4 but with different order",
+			network: mustParseCIDR("192.168.0.0/16"),
+			subnets: []net.IPNet{
+				mustParseCIDR("192.168.1.0/24"),
+				mustParseCIDR("192.168.2.0/24"),
+				mustParseCIDR("192.168.3.0/24"),
+				mustParseCIDR("192.168.3.0/24"),
+			},
+			expectedSubnets: []net.IPNet{
+				mustParseCIDR("192.168.1.0/24"),
+				mustParseCIDR("192.168.2.0/24"),
+				mustParseCIDR("192.168.3.0/24"),
+			},
+		},
+		{
+			name:    "case 6: same as case 4 but with different order",
+			network: mustParseCIDR("192.168.0.0/16"),
+			subnets: []net.IPNet{
+				mustParseCIDR("192.168.1.0/24"),
+				mustParseCIDR("192.168.1.0/24"),
+				mustParseCIDR("192.168.2.0/24"),
+				mustParseCIDR("192.168.3.0/24"),
+			},
+			expectedSubnets: []net.IPNet{
+				mustParseCIDR("192.168.1.0/24"),
+				mustParseCIDR("192.168.2.0/24"),
+				mustParseCIDR("192.168.3.0/24"),
+			},
+		},
+		{
+			name:    "case 7: same as case 4 but with different order",
+			network: mustParseCIDR("192.168.0.0/16"),
+			subnets: []net.IPNet{
+				mustParseCIDR("192.168.1.0/24"),
+				mustParseCIDR("192.168.2.0/24"),
+				mustParseCIDR("192.168.3.0/24"),
+				mustParseCIDR("192.168.1.0/24"),
+			},
+			expectedSubnets: []net.IPNet{
+				mustParseCIDR("192.168.1.0/24"),
+				mustParseCIDR("192.168.2.0/24"),
+				mustParseCIDR("192.168.3.0/24"),
+			},
+		},
+		{
+			name:    "case 7: deduplicate list of subnets with fiveelements where two overlap",
+			network: mustParseCIDR("192.168.0.0/16"),
+			subnets: []net.IPNet{
+				mustParseCIDR("192.168.1.0/24"),
+				mustParseCIDR("192.168.2.0/24"),
+				mustParseCIDR("192.168.3.0/24"),
+				mustParseCIDR("192.168.1.0/24"),
+				mustParseCIDR("192.168.4.0/24"),
+			},
+			expectedSubnets: []net.IPNet{
+				mustParseCIDR("192.168.1.0/24"),
+				mustParseCIDR("192.168.2.0/24"),
+				mustParseCIDR("192.168.3.0/24"),
+				mustParseCIDR("192.168.4.0/24"),
+			},
+		},
+		{
+			name:    "case 8: deduplicate list of subnets with duplicates and IPs from different segments",
+			network: mustParseCIDR("192.168.0.0/16"),
+			subnets: []net.IPNet{
+				mustParseCIDR("192.168.1.0/24"),
+				mustParseCIDR("192.168.2.0/24"),
+				mustParseCIDR("192.168.3.0/24"),
+				mustParseCIDR("192.168.1.0/24"),
+				mustParseCIDR("192.168.4.0/24"),
+				mustParseCIDR("172.31.0.1/16"),
+				mustParseCIDR("10.2.0.4/24"),
+			},
+			expectedSubnets: []net.IPNet{
+				mustParseCIDR("192.168.1.0/24"),
+				mustParseCIDR("192.168.2.0/24"),
+				mustParseCIDR("192.168.3.0/24"),
+				mustParseCIDR("192.168.4.0/24"),
+			},
+		},
+		{
+			name:    "case 9: ensure that smaller overlapping subnets are accepted",
+			network: mustParseCIDR("10.0.0.0/8"),
+			subnets: []net.IPNet{
+				mustParseCIDR("10.1.0.0/16"),
+				mustParseCIDR("10.1.0.0/24"),
+				mustParseCIDR("10.1.1.0/24"),
+			},
+			expectedSubnets: []net.IPNet{
+				mustParseCIDR("10.1.0.0/16"),
+				mustParseCIDR("10.1.0.0/24"),
+				mustParseCIDR("10.1.1.0/24"),
+			},
+		},
+		{
+			name:    "case 10: same as case 9 but with different input ordering",
+			network: mustParseCIDR("10.0.0.0/8"),
+			subnets: []net.IPNet{
+				mustParseCIDR("10.1.0.0/24"),
+				mustParseCIDR("10.1.1.0/24"),
+				mustParseCIDR("10.1.0.0/16"),
+			},
+			expectedSubnets: []net.IPNet{
+				mustParseCIDR("10.1.0.0/24"),
+				mustParseCIDR("10.1.1.0/24"),
+				mustParseCIDR("10.1.0.0/16"),
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			subnets := CanonicalizeSubnets(tc.network, tc.subnets)
+
+			if !reflect.DeepEqual(subnets, tc.expectedSubnets) {
+				msg := "expected: {\n"
+				for _, n := range tc.expectedSubnets {
+					msg += fmt.Sprintf("\t%s,\n", n.String())
+				}
+				msg += "}\n\ngot: {\n"
+				for _, n := range subnets {
+					msg += fmt.Sprintf("\t%s,\n", n.String())
+				}
+				msg += "}"
+				t.Fatal(msg)
+			}
+		})
+	}
+}
+
+func mustParseCIDR(val string) net.IPNet {
+	_, n, err := net.ParseCIDR(val)
+	if err != nil {
+		panic(err)
+	}
+
+	return *n
+}
