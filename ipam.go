62c02421dd40d6640326344eafc1dcbd9e037ada
Tim Schindler <xh3b4sd@gmail.com>
2019-07-09 17:08:33 +0200

    move code from aws-operator to ipam package

--- ipam.go
+++ ipam.go
@@ -13,6 +13,23 @@ import (
 	"github.com/giantswarm/microerror"
 )
 
+// CalculateSubnetMask calculates new subnet mask to accommodate n subnets.
+func CalculateSubnetMask(networkMask net.IPMask, n uint) (net.IPMask, error) {
+	if n == 0 {
+		return nil, microerror.Maskf(invalidParameterError, "divide by zero")
+	}
+
+	// Calculate amount of bits needed to accommodate at least N subnets.
+	subnetBitsNeeded := bits.Len(n - 1)
+
+	maskOnes, maskBits := networkMask.Size()
+	if subnetBitsNeeded > maskBits-maskOnes {
+		return nil, microerror.Maskf(invalidParameterError, "no room in network mask %s to accommodate %d subnets", networkMask.String(), n)
+	}
+
+	return net.CIDRMask(maskOnes+subnetBitsNeeded, maskBits), nil
+}
+
 // CanonicalizeSubnets iterates over subnets and returns deduplicated list of
 // networks that belong to networkRange. Subnets that overlap each other but
 // aren't exactly the same are not removed. Subnets are returned in the same
@@ -140,6 +157,26 @@ func Half(network net.IPNet) (first, second net.IPNet, err error) {
 	}
 
 	return first, second, nil
+}
+
+// Split returns n subnets from network.
+func Split(network net.IPNet, n uint) ([]net.IPNet, error) {
+	mask, err := CalculateSubnetMask(network.Mask, n)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	var subnets []net.IPNet
+	for i := uint(0); i < n; i++ {
+		subnet, err := Free(network, mask, subnets)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+
+		subnets = append(subnets, subnet)
+	}
+
+	return subnets, nil
 }
 
 // add increments the given IP by the number.
